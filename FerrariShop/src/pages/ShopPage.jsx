1550fce7fb2b2747652759f735fca977af0a8dfc
Pabl0Parra <pabsblue16@gmail.com>
2022-10-25 22:42:51 +0200

    rendering cars on shop page

--- FerrariShop/src/pages/ShopPage.jsx
+++ FerrariShop/src/pages/ShopPage.jsx
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from "react";
+import CARS from "../CarsData";
 
 function Shop() {
-  return <div className="shopContainer">shop</div>;
+  return (
+    <div className="shop-container">
+      {CARS.map((car) => {
+        return (
+          <div className="car-container" key={car.id}>
+            <img className="car-image" src={car.image} alt="car"></img>
+            <h3 className="car-name">{car.name}</h3>
+            <p className="car-description">{car.description}</p>
+            <div className="addToCart-div">
+              <button className="addToCart-button">Add To Cart</button>
+              <div className="addToCart-amount">
+                <button className="addToCart-amount-button">-</button>
+                <span>1</span>
+                <button className="addToCart-button">+</button>
+              </div>
+            </div>
+            <div className="line"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Shop;
