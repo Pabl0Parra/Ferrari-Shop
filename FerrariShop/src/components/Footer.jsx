82888422947a1bd7a7fdc5e0338e4903c2a907d9
Pabl0Parra <pabsblue16@gmail.com>
2022-10-25 22:31:17 +0200

    created footer & navbar

---
+++ FerrariShop/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Footer() {
+  return (
+    <div className="footerDiv">
+      Made with ❤️ by <a href="https://github.com/Pabl0Parra">Pabl0Parra</a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ FerrariShop/src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="nav-container">
+      <div className="header-left-div">
+        <h2>Ferrari</h2>
+        <img
+          className="ferrari-logo"
+          alt="logo"
+          src="https://www.carlogos.org/car-logos/ferrari-logo-750x1100.png"
+        ></img>
+      </div>
+      <div className="links-div">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="shop">
+          Shop
+        </Link>
+        <Link className="nav-link" to="about">
+          About
+        </Link>
+        <button className="cart-button">
+          <img
+            src="https://img.icons8.com/dotty/45/000000/shopping-cart.png"
+            alt="cart"
+            className="cart-logo"
+          ></img>
+          <span className="cart-count">0</span>
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
