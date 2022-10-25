53a566c4d79ae162080a537dd56856f6b71870a5
Pabl0Parra <pabsblue16@gmail.com>
2022-10-25 22:31:03 +0200

    added browser router set up

--- FerrariShop/src/App.jsx
+++ FerrariShop/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav";
+import About from "./pages/AboutPage";
+import Shop from "./components/shop";
+import Home from "./components/home";
+import Error from "./components/errorPage";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
